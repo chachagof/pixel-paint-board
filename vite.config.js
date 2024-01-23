08fc4d840198eb5553a397656736980d6f26a005
chachagof <chacha0932@gmail.com>
2024-01-23 20:32:06 +0800

    feat:更改部屬資料

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/pixel-paint-board/',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  base: '/pixel-paint-board/'
 })
