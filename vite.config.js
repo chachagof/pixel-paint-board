77d8b87fa3aa563b00d505a2f8124248419fca26
chachagof <chacha0932@gmail.com>
2024-01-29 22:39:46 +0800

    feat:更改部屬資料

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,3 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -8,10 +6,5 @@ export default defineConfig({
   plugins: [
     vue()
   ],
-  base: 'pixel-paint-board',
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+  base: 'pixel-paint-board'
 })
